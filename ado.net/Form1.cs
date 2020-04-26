7aba5a27b3bd340bdc4e3fe52c3abb6cfdc82375
Tamer <ctamerguven@gmail.com>
2020-04-26 20:21:40 +0300

    Add files via upload

---
+++ ado.net/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace deneme2
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter da;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void doldur() 
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["dbOkul"].ConnectionString;
+            baglanti = new SqlConnection(connectionString);
+            baglanti.Open();
+            da = new SqlDataAdapter("Select *From ogrenci", baglanti);
+            DataTable tablo = new DataTable();
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            doldur();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sorgu = "Insert into ogrenci (Numara, Ad, Soyad) values (@Numara,@Ad,@Soyad)";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@Numara", textBox1.Text);
+            komut.Parameters.AddWithValue("@Ad", textBox2.Text);
+            komut.Parameters.AddWithValue("@Soyad", textBox3.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            doldur();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string sorgu = "Update ogrenci Set Numara=@Numara,Ad=@Ad,Soyad=@Soyad Where Numara=@Numara";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@Numara", Convert.ToInt32(textBox1.Text));
+            komut.Parameters.AddWithValue("@Ad", textBox2.Text);
+            komut.Parameters.AddWithValue("@Soyad", textBox3.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            doldur();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string sorgu = "Delete From ogrenci Where Numara=@Numara";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@Numara", Convert.ToInt32(textBox1.Text));
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            doldur();
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+        }
+    }
+}
---
+++ entity/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace deneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        dbOkulEntities db;
+        void doldur()  
+        {
+            db = new dbOkulEntities();
+            dataGridView1.DataSource = db.ogrencis.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ogrenci yeniogrenci = new ogrenci();
+            yeniogrenci.Numara = Convert.ToInt32(textBox1.Text);
+            yeniogrenci.Ad = textBox2.Text;
+            yeniogrenci.Soyad = textBox3.Text;
+            db.ogrencis.Add(yeniogrenci);
+            db.SaveChanges();
+            doldur();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int guncelle = Convert.ToInt32(textBox1.Text);
+            var guncellenecekogrenci = db.ogrencis.Where(w => w.Numara == guncelle).FirstOrDefault();
+            guncellenecekogrenci.Ad = textBox2.Text;
+            guncellenecekogrenci.Soyad = textBox3.Text;
+            db.SaveChanges();
+            doldur();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int silinecek = Convert.ToInt32(textBox1.Text);
+            var silinecekkisi = db.ogrencis.Where(w => w.Numara == silinecek).FirstOrDefault();
+            db.ogrencis.Remove(silinecekkisi);
+            db.SaveChanges();
+            doldur();
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            doldur();
+        }
+    }
+}
